68394ecd62297b8e3381d886bf8aa4235d23edbe
UzanMuyumba <19um518@esisalama.org>
2024-05-24 22:56:57 +0200

    Add files via upload (still tweaking)
    Migration de la classe User et creation du fichier base.html

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField(help_text="L'adresse mail")
+    pseudo = models.CharField(max_length=50, help_text="Le pseudo")
+    password = models.CharField(max_length=16, help_text="Le mdp")
