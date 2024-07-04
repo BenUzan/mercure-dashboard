85783b2c76f94e9a03ce8120bbb27c49ba6daa53
UzanMuyumba <19um518@esisalama.org>
2024-07-04 23:17:46 +0200

    Add files via upload (still tweaking)

--- src/mercure/settings.py
+++ src/mercure/settings.py
@@ -31,14 +31,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    #* My Apps
-    'main',
+    # * My Apps
+    'store',
     'accounts',
-    
-    #* Third Party Apps
-    'django_bootstrap5',
-    
-    #* Default Django Apps
+
+    # * Third Party Apps
+
+    # * Default Django Apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -64,9 +63,9 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            
+
             BASE_DIR / "templates"
-            ],
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -132,6 +131,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
