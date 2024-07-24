7efcc43d51c9ee7c7a26b97ced7d431335cf3e8b
UzanMuyumba <19um518@esisalama.org>
2024-07-24 22:11:05 +0200

    Add files via upload (still tweaking)

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -1,9 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
 class User(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
+    # phone_number = ...
     email = models.EmailField(help_text="L'adresse mail")
     username = models.CharField(max_length=50, help_text="Le pseudo")
     password = models.CharField(max_length=16, help_text="Le mdp")
