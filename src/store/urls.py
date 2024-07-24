3506d54e3800f86c0b4049161db969a50fb28c4f
UzanMuyumba <19um518@esisalama.org>
2024-07-24 22:12:23 +0200

    Update urls.py

--- src/store/urls.py
+++ src/store/urls.py
@@ -3,5 +3,13 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
-    path('rapport/', views.rapport, name='rapport'),
+    path('daily-report/', views.report, name='daily-report'),
+    path('create-product/', views.create_product, name='cp'),
+    path('create-order/', views.create_order, name='create_order'),
+    path('product-list/', views.product_list, name='product-list'),
+    path('product-list/up/<int:id>/', views.update_product, name='up'),
+    path('product-list/up/upr/<int:id>/',
+         views.update_product_record, name='upr'),
+    path("product-list/dp/<int:id>/", views.delete_product, name="dp")
+
 ]
