bef6c0a85a28eb527a17e4e87ec21e20d45c99fc
UzanMuyumba <19um518@esisalama.org>
2024-08-07 16:05:10 +0200

    Delete files

--- src/accounts/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("signup/", views.signupaccount, name="signup"),
-    path("login/", views.loginaccount, name="login"),
-    path("logout/", views.logoutaccount, name="logout")
-]
